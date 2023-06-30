a073b21c5e6128eb7d9dd4b9c40688d96118ceb9
Rayaan013 <84763623+Rayaan013@users.noreply.github.com>
2023-06-30 16:31:13 +0530

    Add files via upload

---
+++ authentication/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path
+from authentication.views import home,signup,signin,signout
+
+urlpatterns = [
+        path('',home,name="home"),
+        path('signup',signup,name="signup"),
+        path('signin',signin,name="signin"),
+        path('signout',signout,name="signout")
+    ]
---
+++ loginsystem/info.py
@@ -0,0 +1,5 @@
+EMAIL_USE_TLS=True
+EMAIL_HOST='smtp.gmail.com'
+EMAIL_HOST_USER='rayaanmr2000@gmail.com'
+EMAIL_HOST_PASSWORD='Rayaan@novoresume123'
+EMAIL_PORT=587
